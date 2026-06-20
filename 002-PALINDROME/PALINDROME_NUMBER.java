2ea16117caaa29829131cab3d94bad8162162174
NAVABALAN T <navatj30@gmail.com>
2026-06-20 21:39:05 +0530

    Update PALINDROME_NUMBER.java

--- 002-PALINDROME/PALINDROME_NUMBER.java
+++ 002-PALINDROME/PALINDROME_NUMBER.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isPalindrome(int x) {
         int t=x;
         int rev=0;
+        if (x < 0) return false;
         while(x>0){
             int a=x%10;
             rev=rev*10+a;
